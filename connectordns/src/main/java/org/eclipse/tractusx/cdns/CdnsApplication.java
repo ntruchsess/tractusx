84f3803797f1ddcc47bad20b6487675c0500b036
martin <martin@raepple.net>
2021-06-03 20:40:01 +0200

    Fixed container tag in pipeline

--- connectordns/src/main/java/org/eclipse/tractusx/cdns/CdnsApplication.java
+++ connectordns/src/main/java/org/eclipse/tractusx/cdns/CdnsApplication.java
@@ -11,7 +11,7 @@ public class CdnsApplication {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello from the CDNS Service!";
+		return "Hello from the CDNS Service!!!";
 	}
 
 	public static void main(String[] args) {
