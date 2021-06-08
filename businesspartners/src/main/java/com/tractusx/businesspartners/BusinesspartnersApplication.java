7d8110dc055478654f34643a1846377077540398
cookienowskyj <mkoko@live.de>
2021-06-08 20:37:13 +0200

    adde table storage access
    Added classes to access the tabelstorage and query busines partners

--- businesspartners/src/main/java/com/tractusx/businesspartners/BusinesspartnersApplication.java
+++ businesspartners/src/main/java/com/tractusx/businesspartners/BusinesspartnersApplication.java
@@ -1,10 +1,12 @@
 package com.tractusx.businesspartners;
 
+import com.tractusx.businesspartners.controllers.tablestorage.TableStorageConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BusinesspartnersApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(BusinesspartnersApplication.class, args);
