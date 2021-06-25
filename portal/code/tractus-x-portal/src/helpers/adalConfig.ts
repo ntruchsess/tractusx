120b766a56167630234a817d7e2b8372947c7018
Paul Tallett <paul_tallett@hotmail.com>
2021-06-25 17:47:21 +0100

    Hook up AAD tenant

--- portal/code/tractus-x-portal/src/helpers/adalConfig.ts
+++ portal/code/tractus-x-portal/src/helpers/adalConfig.ts
@@ -7,8 +7,9 @@
 //
 
 import { AdalConfig, adalGetToken, AuthenticationContext } from 'react-adal';
+
 // Endpoint URL
-export const endpoint = 'https://tractusx.azurewbsites.net/';
+export const endpoint = 'https://catenax.azurewbsites.net/';
 
 export function getReplyUrl() {
   const url = new URL(window.location.href);
@@ -21,7 +22,7 @@ export function getReplyUrl() {
 }
 
 // App Registration ID bb01c83a-f70e-44d9-a0ac-f6b3b70c1775
-const appId = 'bb01c83a-f70e-44d9-a0ac-f6b3b70c1775';
+const appId = '4c6269ac-8106-4bbb-b0d9-6c36a6e3b131';
 export const adalConfig: AdalConfig = {
   cacheLocation: 'localStorage',
   clientId: appId,
@@ -31,45 +32,70 @@ export const adalConfig: AdalConfig = {
   redirectUri: getReplyUrl(),
   // popUp: true,
   postLogoutRedirectUri: window.location.origin,
-  tenant: 'microsoft.com'
+  tenant: 'catenaxpoc.onmicrosoft.com'
 };
 
 class AdalContext {
-    private readonly authContext: AuthenticationContext;
-    private readonly isAdmin: boolean = false;
-    constructor() {
-      this.authContext = new AuthenticationContext(adalConfig);
-    }
+  private readonly authContext: AuthenticationContext;
+  private readonly isAdmin: boolean = false;
+  constructor() {
+    this.authContext = new AuthenticationContext(adalConfig);
+  }
 
-    get AuthContext() {
-        return this.authContext;
-    }
+  get AuthContext() {
+    return this.authContext;
+  }
 
-    get IsAdmin() {
-        return this.isAdmin;
-    }
+  get IsAdmin() {
+    return this.isAdmin;
+  }
 
-    public getToken(): Promise<string | null> {
-        return adalGetToken(this.authContext, endpoint);
-    }
-
-    public getCachedToken(): string {
-        return this.authContext.getCachedToken(this.authContext.config.clientId);
-    }
-
-    public getUsername(): string {
-        let username = '';
-        const user = this.authContext.getCachedUser();
-        if (user) {
-            username = user.userName;
+  public acquireToken(resource?: string): Promise<string> {
+    resource = resource || appId;
+    const promise = new Promise<string>((resolve, reject) => {
+      const user = this.AuthContext.getCachedUser();
+      if (user) {
+        this.authContext.config.extraQueryParameter = 'login_hint=' + (user.profile.upn || user.userName);
+      }
+  
+      adalGetToken(this.authContext, resource).then((token) => {
+        resolve(token);
+      }, (error) => {
+        if (error) {
+          console.log(error);
+          if (error.msg === 'login_required' || error.msg === 'interaction_required') {
+            this.authContext.login();
+          } else {
+            // AlertDialog.(error.message);
+          }
         }
+      });
+    });
+  
+    return promise;
+  }
+  
+  public getToken(): Promise<string | null> {
+    return adalGetToken(this.authContext, endpoint);
+  }
 
-        return username;
+  public getCachedToken(): string {
+    return this.authContext.getCachedToken(this.authContext.config.clientId);
+  }
+
+  public getUsername(): string {
+    let username = '';
+    const user = this.authContext.getCachedUser();
+    if (user) {
+      username = user.userName;
     }
 
-    public logOut() {
-        this.authContext.logOut();
-    }
+    return username;
+  }
+
+  public logOut() {
+    this.authContext.logOut();
+  }
 }
 
 const adalContext: AdalContext = new AdalContext();
