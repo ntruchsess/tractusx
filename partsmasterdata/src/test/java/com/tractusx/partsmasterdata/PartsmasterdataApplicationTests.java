31a2ae7d7f91888194aa79def69fdc1bfe6c76ff
Matthias Tandler <matand@microsoft.com>
2021-06-28 16:58:23 +0200

    Initial partsmaster

---
+++ partsmasterdata/src/test/java/com/tractusx/partsmasterdata/PartsmasterdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tractusx.partsmasterdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartsmasterdataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
