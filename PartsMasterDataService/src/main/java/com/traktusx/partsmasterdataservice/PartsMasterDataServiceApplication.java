e996029fde5d46e77068bca3b839fa410c78e22a
Matthias Tandler <matand@microsoft.com>
2021-06-29 15:29:55 +0200

    interim create PartsMasterDataServiceApplication

---
+++ PartsMasterDataService/src/main/java/com/traktusx/partsmasterdataservice/PartsMasterDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.traktusx.partsmasterdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartsMasterDataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartsMasterDataServiceApplication.class, args);
+    }
+
+}
