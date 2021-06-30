a56c812d1785b4690a9dc1e384f153b3718fb5fe
Matthias Tandler <matand@microsoft.com>
2021-06-30 19:19:15 +0200

    test of config injection

--- uploadAppAdapter/src/main/java/com/tractusx/uploadappadapter/controllers/UploadAppAdapterController.java
+++ uploadAppAdapter/src/main/java/com/tractusx/uploadappadapter/controllers/UploadAppAdapterController.java
@@ -26,6 +26,12 @@ public class UploadAppAdapterController {
         return dbAccess.GetPartsFromDatabase(companyOneId);
     }
 
+    @GetMapping("api/testParams")
+    public String testParams()
+    {
+        return blobConfig.storageConnectionstring;
+    }
+
     @PostMapping("/api/upload")
     public String handleFileUpload(@RequestParam("file")MultipartFile file, @RequestParam String company){
         String retVal;
