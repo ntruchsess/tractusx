268a69abf17a24051009573eafce7d78ed21f068
Matthias Tandler <matand@microsoft.com>
2021-07-02 10:32:26 +0200

    partsmaster move

---
+++ coreservices/partsmasterdata/src/main/java/com/tractusx/partsmasterdata/models/PartPartTree.java
@@ -0,0 +1,18 @@
+package com.tractusx.partsmasterdata.models;
+
+public class PartPartTree {
+    //type: array
+    //items:
+    //type: string
+    //example: G05
+    //description: UID, UID
+    public String[] isParentOf;
+
+    public void setIsParentOf(String[] isParentOf) {
+        this.isParentOf = isParentOf;
+    }
+
+    public String[] getQualityAlert() {
+        return isParentOf;
+    }
+}
