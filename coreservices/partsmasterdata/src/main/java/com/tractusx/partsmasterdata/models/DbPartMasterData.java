21e4ac6b069124e16b4cf2192b30fd2630937b7d
Matthias Tandler <matand@microsoft.com>
2021-07-05 16:07:32 +0200

    Added db methods

---
+++ coreservices/partsmasterdata/src/main/java/com/tractusx/partsmasterdata/models/DbPartMasterData.java
@@ -0,0 +1,35 @@
+package com.tractusx.partsmasterdata.models;
+
+import java.sql.Timestamp;
+
+public class DbPartMasterData extends PartMasterData{
+    public int id;
+    public Timestamp validUntilUtc;
+    public Boolean isTempPart;
+
+    public int getId() {
+        return id;
+    }
+
+    public Timestamp getValidUntilUtc() {
+        return validUntilUtc;
+    }
+
+    public Boolean getIsTempPart()
+    {
+        return isTempPart;
+    }
+
+    public void setId(int idObject) {
+        this.id = idObject;
+    }
+
+    public void setValidUntilUtc(Timestamp validUntilUtcObject) {
+        this.validUntilUtc = validUntilUtcObject;
+    }
+
+    public void setIsTempPart(Boolean isTempPartObject){
+        this.isTempPart = isTempPartObject;
+    }
+
+}
