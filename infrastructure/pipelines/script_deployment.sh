768fe156f5790a7bae9c02d4ced54d24afd4b825
Dr. Christoph 'Schorsch' Jung <c-jung@t-systems.com>
2021-09-03 15:06:28 +0200

    #CATART0PO6-27 Implement envsubst kubernetes deployments.

--- infrastructure/pipelines/script_deployment.sh
+++ infrastructure/pipelines/script_deployment.sh
@@ -14,7 +14,7 @@
 #
 # Secrets and environmental informations need to be set via environment variables
 ##Docker Registry
-#export REGISTRY=tsicatenaxdevacr.azurecr.io
+#export CONTAINER_REGISTRY=catenaxdevacr.azurecr.io
 ##Image Versions
 #export VERSION=latest
 ##User for service/servers
@@ -35,11 +35,14 @@
 #export PORTAL=tsicatenaxdevaksportalsrv
 ## Application ID as registered in AAD
 #export APIID=
+#export CLUSTER_ISSUER=letsencrypt-prod
+#export CATENA_SERVICE_URL=catenaxdevakssrv.germanywestcentral.cloudapp.azure.com
+#export IMAGE_PULL_POLICY=Always
 
 cd ../../coreservices/connectordns
 mvn install -DskipTests
-docker build -t $REGISTRY/cdns:$VERSION .
-docker push $REGISTRY/cdns:$VERSION
+docker build -t $CONTAINER_REGISTRY/cdns:$VERSION .
+docker push $CONTAINER_REGISTRY/cdns:$VERSION
 
 cd ../../infrastructure/pipelines
 
@@ -48,15 +51,15 @@ kubectl create namespace cdns
 kubectl create secret generic cdns-secret -n cdns --from-literal=http_basic_auth_password=${HTTPPASSWORD} --from-literal=storage_connectionstring=${STORAGEACCOUNT_CONNECTIONSTRING} --dry-run=client -o yaml \
     | kubectl apply -f -
 
-kubectl apply -f ../manifests/cdns.yaml -n cdns
+cat ../manifests/cdns.yaml | envsubst | kubectl apply -n cdns
 
-kubectl apply -f ../manifests/cdns-ingress.yaml -n cdns
+cat ../manifests/cdns-ingress.yaml | envsubst | kubectl apply -n cdns
 
 cd ../../coreservices/businesspartners
 
 mvn package -DskipTests
-docker build -t $REGISTRY/businesspartners:$VERSION .
-docker push $REGISTRY/businesspartners:$VERSION
+docker build -t $CONTAINER_REGISTRY/businesspartners:$VERSION .
+docker push $CONTAINER_REGISTRY/businesspartners:$VERSION
 
 cd ../../infrastructure/pipelines
 
@@ -65,19 +68,15 @@ kubectl create namespace businesspartners
 kubectl create secret generic businesspartners-secret -n businesspartners --from-literal=http_basic_auth_password=${HTTPPASSWORD} --from-literal=storage_connectionstring=${STORAGEACCOUNT_CONNECTIONSTRING} --dry-run=client -o yaml \
     | kubectl apply -f -
 
-kubectl apply -f ../manifests/businesspartners.yaml -n businesspartners
+cat ../manifests/businesspartners.yaml | envsubst | kubectl apply -n businesspartners
 
-kubectl apply -f ../manifests/businesspartners-ingress.yaml -n businesspartners
+cat ../manifests/businesspartners-ingress.yaml | envsubst | kubectl apply -n businesspartners
 
 cd ../../coreservices/partsmasterdata
 mvn package -DskipTests
-docker build -t $REGISTRY/partsmasterdata:$VERSION .
-docker push $REGISTRY/partsmasterdata:$VERSION
+docker build -t $CONTAINER_REGISTRY/partsmasterdata:$VERSION .
+docker push $CONTAINER_REGISTRY/partsmasterdata:$VERSION
 
-cd ../../coreservices/kmuuploadapp
-mvn package -DskipTests
-docker build -t $REGISTRY/uploadappadapter:$VERSION .
-docker push $REGISTRY/uploadappadapter:$VERSION
 
 cd ../../infrastructure/pipelines
 
@@ -86,23 +85,28 @@ kubectl create namespace partsmasterdata
 kubectl create secret generic partsmasterdata-secret -n partsmasterdata --from-literal=http_basic_auth_password=${HTTPPASSWORD} --from-literal=postgres_partsmaster_url=${POSTGREPARTSMASTERURL} --from-literal=postgre_partsmaster_user=${HTTPUSER}  --from-literal=postgre_partsmaster_password=${POSTGREPARTSMASTERPASSWORD} --from-literal=postgre_partsmaster_db=partsmaster  --dry-run=client -o yaml \
     | kubectl apply -f -
 
-kubectl apply -f ../manifests/partsmasterdata.yaml -n partsmasterdata
+cat ../manifests/partsmasterdata.yaml | envsubst | kubectl apply -n partsmasterdata
 
-kubectl apply -f ../manifests/partsmasterdata-ingress.yaml -n partsmasterdata
+cat ../manifests/partsmasterdata-ingress.yaml | envsubst | kubectl apply -n partsmasterdata
+
+cd ../../coreservices/kmuuploadapp
+mvn package -DskipTests
+docker build -t $CONTAINER_REGISTRY/uploadappadapter:$VERSION .
+docker push $CONTAINER_REGISTRY/uploadappadapter:$VERSION
 
 cd ../../infrastructure/pipelines
 
 kubectl create secret generic uploadappadapter-secret -n partsmasterdata --from-literal=storage_connectionstring=${STORAGEACCOUNT_CONNECTIONSTRING} --from-literal=postgre_upload_user=${HTTPUSER} --from-literal=http_basic_auth_password=${HTTPPASSWORD}  --from-literal=postgres_upload_url=${POSTGREPARTSMASTERURL} --from-literal=postgre_upload_password=${POSTGREPARTSMASTERPASSWORD} --from-literal=postgre_upload_db=upload --from-literal=aad_api_id_uri=${APIID} --from-literal=aad_client_id=${APIID}  --dry-run=client -o yaml \
     | kubectl apply -f -
 
-kubectl apply -f ../manifests/uploadapp.yaml -n partsmasterdata
+cat ../manifests/uploadapp.yaml | envsubst | kubectl apply -n partsmasterdata
 
-kubectl apply -f ../manifests/uploadapp-ingress.yaml -n partsmasterdata
+cat ../manifests/uploadapp-ingress.yaml | envsubst | kubectl apply -n partsmasterdata
 
 cd ../../coreservices/simplescheduler
 mvn package -DskipTests
-docker build -t $REGISTRY/simplescheduler:$VERSION .
-docker push $REGISTRY/simplescheduler:$VERSION
+docker build -t $CONTAINER_REGISTRY/simplescheduler:$VERSION .
+docker push $CONTAINER_REGISTRY/simplescheduler:$VERSION
 
 cd ../../infrastructure/pipelines
 
@@ -111,53 +115,39 @@ kubectl create namespace centralconnector
 kubectl create secret generic simplescheduler-secret -n centralconnector --from-literal=connector_login=${HTTPUSER} --from-literal=connector_password=${HTTPPASSWORD}  --dry-run=client -o yaml \
     | kubectl apply -f -
 
-kubectl apply -f ../manifests/simplescheduler.yaml -n centralconnector
+cat ../manifests/simplescheduler.yaml |envsubst | kubectl apply -n centralconnector
 
 cd ../../portal/code/tractus-x-portal
 
-docker build --build-arg HTTP_PROXY=$PROXY --build-arg HTTPS_PROXY=$PROXY -t $REGISTRY/portal:$VERSION .
-docker push $REGISTRY/portal:$VERSION
+docker build -f Dockerfile.develop --build-arg HTTP_PROXY=$PROXY --build-arg HTTPS_PROXY=$PROXY -t $CONTAINER_REGISTRY/portal:$VERSION .
+docker push $CONTAINER_REGISTRY/portal:$VERSION
 
 cd ../../../infrastructure/pipelines
 
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 
---set controller.nodeSelector."kubernetes\.io/os"=linux \
-    --set controller.image.registry=$ACR_URL \
-    --set controller.image.image=$CONTROLLER_IMAGE \
-    --set controller.image.tag=$CONTROLLER_TAG \
-    --set controller.image.digest="" \
-    --set controller.admissionWebhooks.patch.nodeSelector."kubernetes\.io/os"=linux \
-    --set controller.admissionWebhooks.patch.image.registry=$ACR_URL \
-    --set controller.admissionWebhooks.patch.image.image=$PATCH_IMAGE \
-    --set controller.admissionWebhooks.patch.image.tag=$PATCH_TAG \
-    --set defaultBackend.nodeSelector."kubernetes\.io/os"=linux \
-    --set defaultBackend.image.registry=$ACR_URL \
-    --set defaultBackend.image.image=$DEFAULTBACKEND_IMAGE \
-    --set defaultBackend.image.tag=$DEFAULTBACKEND_TAG \
-    
 kubectl create namespace ingress-portal
 
 helm install nginx-ingress ingress-nginx/ingress-nginx \
     --namespace ingress-portal \
     --set controller.replicaCount=1 \
-    --set controller.service.loadBalancerIP=20.79.116.193 \
-    --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-dns-label-name"=$PORTAL \
+    --set controller.service.loadBalancerIP=$PORTAL_IP \
+    --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-dns-label-name"=$PORTAL_DOMAIN \
     --set controller.ingressClass=nginx-portal 
 
 kubectl create namespace portal
 
-kubectl apply -f ../manifests/portal.yaml -n portal
+cat ../manifests/portal.yaml | envsubst | kubectl apply -n portal
 
 kubectl rollout restart deployment portal -n portal
 
-kubectl apply -f ../manifests/portal-ingress.yaml -n portal
+cat ../manifests/portal-ingress.yaml | envsubst | kubectl apply -n portal
 
 cd ../../semantics
 mvn package -DskipTests
-docker build -t $REGISTRY/semantics:$VERSION .
-docker push $REGISTRY/semantics:$VERSION
+docker build -t $CONTAINER_REGISTRY/semantics:$VERSION .
+docker push $CONTAINER_REGISTRY/semantics:$VERSION
 
 cd ../infrastructure/pipelines
 
