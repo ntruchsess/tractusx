27d1d2b115de82178d08ff10e179f7bcf1416e12
Dr. Christoph 'Schorsch' Jung <c-jung@t-systems.com>
2021-09-10 08:01:09 +0200

    #CATART0PO6-5 Document the environment.sh including the necessary secrets.

--- infrastructure/manifests/environment.sh
+++ infrastructure/manifests/environment.sh
@@ -8,21 +8,52 @@
 # additional information regarding license terms.
 #
 
-# Script to set a k8/manifest environment
-# productive issuer, set to -staging if authority needs not to be the productive one
+# Script to set a k8/manifest environment (here: PoC/Speedboat)
+# This version contains no secrets (see the commented enties below) which need to be
+# added best throuhgh a seperate secrets.sh (ignored in git)
+
+# certificate stuff, here: productive issuer, set to -staging if authority needs not to be strong
 export CLUSTER_ISSUER=
-export SERVICE_DOMAIN=catenaxdevakssrv
-export CATENA_SERVICE_URL=${SERVICE_DOMAIN}.germanywestcentral.cloudapp.azure.com
-export PORTAL_DOMAIN=catenaxdevaksportalsrv
-export CATENA_PORTAL_URL=${PORTAL_DOMAIN}.germanywestcentral.cloudapp.azure.com
-export PORTAL_IP=20.79.77.83
+export CATENA_ADMIN_MAIL=admin@example.com
+# container stuff
 export CONTAINER_REGISTRY_SHORT=catenaxdevacr
 export CONTAINER_REGISTRY=${CONTAINER_REGISTRY_SHORT}.azurecr.io
-export STORAGE_ACCOUNT_NAME=catenaxstorage
-export POSTGRES_RESOURCE_NAME=catenaxdevdb
 export VERSION=latest
 export IMAGE_PULL_POLICY=Always
-export CATENA_ADMIN_MAIL=admin@example.com
 export NODE_RESOURCE_GROUP=catenax-dev-node-rg
 export K8_RESOURCE_GROUP=catenax-dev-rg
 export K8_RESOURCE_NAME=catenax-dev-aks-services
+# Persistence Layer
+export STORAGE_ACCOUNT_NAME=catenaxstorage
+export POSTGRES_RESOURCE_NAME=catenaxdevdb
+# Service Layer
+export SERVICE_DOMAIN=catenaxdevakssrv
+export CATENA_SERVICE_URL=${SERVICE_DOMAIN}.germanywestcentral.cloudapp.azure.com
+# Portal Layer
+export PORTAL_DOMAIN=catenaxdevaksportalsrv
+export CATENA_PORTAL_URL=${PORTAL_DOMAIN}.germanywestcentral.cloudapp.azure.com
+export PORTAL_IP=20.79.77.83
+
+###
+### Secrets (define in secrets.sh)
+###
+
+# build environment
+#export HTTP_PROXY_HOST=
+#export HTTP_PROXY_PORT=
+#export PROXY=http://${HTTP_PROXY_HOST}:${HTTP_PROXY:PORT}
+
+# certificate secrets
+#export KEYSTORE_PASSWORD=
+
+# SSO secrets
+#export APIID=
+
+# persistence secrets
+#export STORAGEACCOUNT_CONNECTIONSTRING=
+#export POSTGREPARTSMASTERPASSWORD=
+#export POSTGREPARTSMASTERURL="jdbc:postgresql://${DATABASE_RESOURCE_NAME}.postgres.database.azure.com:5432/partsmasterdata?user=${HTTPUSER}@${DATABASE_RESOURCE_NAME}&password=${POSTGREPARTSMASTERPASSWORD}&sslmode=require"
+
+# service & portal layer secrets 
+#export HTTPUSER=
+#export HTTPPASSWORD=
