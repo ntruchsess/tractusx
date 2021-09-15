9fe3ddb4f0064d7feb274a7fec31ab19260f625d
Marc Gomez <marcgomez@microsoft.com>
2021-09-15 17:39:13 +0200

    Implement sample integration test using full server strategy

--- coreservices/partsrelationshipservice/src/test/java/net/catenax/prs/controllers/PrsControllerTests.java
+++ coreservices/partsrelationshipservice/src/test/java/net/catenax/prs/controllers/PrsControllerTests.java
@@ -2,28 +2,25 @@ package net.catenax.prs.controllers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 public class PrsControllerTests {
 
+    @LocalServerPort
+    private int port;
+
     @Autowired
-    private MockMvc mvc;
+    private TestRestTemplate restTemplate;
 
     @Test
-    public void greetingShouldReturnDefaultMessage() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/api/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString(
-                        "Hello World")));
+    public void greetingShouldReturnDefaultMessage() {
+        String response = this.restTemplate.getForObject("http://localhost:" + port + "/api/hello", String.class);
+        assertThat(response).contains("Hello World!");
     }
 }
