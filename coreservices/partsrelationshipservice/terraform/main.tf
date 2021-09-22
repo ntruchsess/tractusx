b28f2c7009a24479cd88b2bb8ec6ac51d2448d59
Ophélie Le Mentec <ophelie@lementec.com>
2021-09-22 11:58:19 +0200

    Fix/cax 1 prs@180 Fixed postgresql.url config (#45)
    * Fixed config.
    
    * Fixed config in terraform config.
    
    * Revert "Fixed config."
    
    This reverts commit 7dffd42f8e0e25c16f8e76beeb372ed533aff9cb.
    
    * Fix.

--- coreservices/partsrelationshipservice/terraform/main.tf
+++ coreservices/partsrelationshipservice/terraform/main.tf
@@ -88,7 +88,7 @@ resource "helm_release" "prs" {
 
   set {
     name  = "postgresql.url"
-    value = module.prs_postgresql.fqdn
+    value = "jdbc:postgresql://${module.prs_postgresql.fqdn}/${module.prs_postgresql.db_name}?sslmode=require"
   }
 
   set {
