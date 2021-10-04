0ecc0e6e505ea27c842cecadedef1de8f95e235a
michael <mkoko@live.de>
2021-10-04 08:39:39 +0200

    added toast

--- portal/code/tractus-x-portal/src/components/registration.tsx
+++ portal/code/tractus-x-portal/src/components/registration.tsx
@@ -16,49 +16,68 @@ import * as React from 'react';
 import { observable } from 'mobx';
 import { observer } from 'mobx-react';
 import { PrimaryButton, TextField } from '@fluentui/react';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+// import 'react-toastify/dist/ReactToastify.minimal.css';
 
 @observer
 export default class Login extends React.Component {
 
-  @observable private email : string = "";
+  @observable private email: string = "";
   @observable private oneId: string = "CAXLZJVJEBYWYYZZ";
-  
+
   private registrationButtonClick() {
-   console.log("register");
+    console.log("register");
 
-   var u = 'http://cxonbiardoing.azurewebsites.net/api/onboarding'
+    var u = 'http://cxonbiardoing.azurewebsites.net/api/onboarding'
 
-    var data = 
-      {
-        "oneId" : this.oneId,
-        "eMail" : this.email
+    var data =
+    {
+      "oneId": this.oneId,
+      "eMail": this.email
+    }
+
+    if (this.email === "" || this.oneId === "") {
+      toast.error('Email or OneId empty.');
+      return;
+    }
+
+
+    console.log(data);
+
+    fetch(u, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(data) })
+      .then((response) => {
+        if (response.ok) {
+          toast.success('Onboarding for company id: ' + this.oneId + ' started.');
         }
-    
-        console.log(data);
-   fetch(u, { method: 'POST', headers: {  'Content-Type': 'application/json' }, body: JSON.stringify(data) } )
-   .then((response) =>
-   console.log(response)
-   );
+        else throw Error();
+      }
+
+      ).catch((error) => {
+        toast.error('Onboarding for company id: ' + this.oneId + ' failed. Company is already registered.')
+      });
 
   }
+
 
   public render() {
     return (
       <div className='w100pc h100pc df fwrap fdc bgregisterimage aic'>
-          <div className='w40pc mt100 df fdc'> 
+        <div className='w40pc mt100 df fdc'>
           <span className='fs60 bold mt20 ml30'>Welcome to Catena-X</span>
           <span className='fs20 bold mt10 ml30'>Automotive network</span>
           <span className='fs14 bold mt30 ml30'>If you have problems, please contact our support</span>
-          </div>
-          <div className="w40pc bgwhite mt100 br7 df fdc bsPanel">
+        </div>
+        <div className="w40pc bgwhite mt100 br7 df fdc bsPanel">
           <span className='fs20 bold mt20 ml50'>Register to Catena-X</span>
           <span className='fs14 m50'>
-                    <TextField placeholder='Enter  Email - Address' className='w100pc br4 pr10 h40' value={this.email} onChange={(ev, val) => this.email = val}  />
-                    <TextField placeholder='One ID' className='w100pc br4 pr10 h40' value={this.oneId} onChange={(ev, val) => this.oneId = val} />
-                    <PrimaryButton className='w60pc br4 pr10 h40 mt20' text='REGISTRATION' onClick={() => this.registrationButtonClick()} />
+            <TextField placeholder='Enter  Email - Address' className='w100pc br4 pr10 h40' value={this.email} onChange={(ev, val) => this.email = val} />
+            <TextField placeholder='One ID' className='w100pc br4 pr10 h40' value={this.oneId} onChange={(ev, val) => this.oneId = val} />
+            <PrimaryButton className='w60pc br4 pr10 h40 mt20' text='REGISTRATION' onClick={() => this.registrationButtonClick()} />
           </span>
-          </div>
+        </div>
+        <ToastContainer />
       </div>
-    )} 
+    )
+  }
 }
