c8611aff2e0c267499e93aa872253111334c924b
Tobias Schwarz <tobias.schwarz@microsoft.com>
2021-10-14 14:30:50 +0200

    added db access for company roles

---
+++ coreservices/onboarding/src/CatenaX.NetworkServices.Onboarding.Service/OnboardingAccess/IOnboardingDBAccess.cs
@@ -0,0 +1,12 @@
+﻿using CatenaX.NetworkServices.Onboarding.Service.Model;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CatenaX.NetworkServices.Onboarding.Service.OnboardingAccess
+{
+    public interface IOnboardingDBAccess
+    {
+        Task<IEnumerable<CompanyRole>> GetAllCompanyRoles();
+    }
+}
