ef8bee3aad66d1cb3139fbbe18182b52359c8c8a
Alexandre Gattiker <algattik@users.noreply.github.com>
2021-11-08 14:37:31 +0100

    Feature/cax 1 mtpdc@111 pom files (#224)
    * .
    
    * .
    
    * .
    
    * flatten
    
    * v
    
    * com.catenax.prs
    
    * .
    
    * refo
    
    * Update Dockerfile
    
    * Update pom.xml
    
    * Update pom.xml
    
    * .
    
    * Added Param
    
    * Removing redundant group id declaration and adding project encoding
    
    Co-authored-by: Peeyush Chandel <555114+cpeeyush@users.noreply.github.com>

--- coreservices/partsrelationshipservice/prs-api/src/main/java/net/catenax/prs/repositories/PartAttributeRepository.java
+++ coreservices/partsrelationshipservice/prs-api/src/main/java/net/catenax/prs/repositories/PartAttributeRepository.java
@@ -14,6 +14,7 @@ import net.catenax.prs.entities.PartAttributeEntityKey;
 import net.catenax.prs.entities.PartIdEntityPart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
 import java.util.List;
@@ -38,5 +39,9 @@ public interface PartAttributeRepository extends JpaRepository<PartAttributeEnti
      * @see org.springframework.data.repository.CrudRepository#findAllById(Iterable)
      */
     @Query("SELECT a FROM PartAttributeEntity a WHERE a.key.partId IN (:partIds) AND a.key.attribute = :name")
-    List<PartAttributeEntity> findAllBy(Collection<PartIdEntityPart> partIds, String name);
+    List<PartAttributeEntity> findAllBy(
+            @Param("partIds")
+            Collection<PartIdEntityPart> partIds,
+            @Param("name")
+            String name);
 }
