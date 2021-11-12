9416eb9c5ad359ac0c05ff601f882e43d254f4cd
Alexandre Gattiker <algattik@users.noreply.github.com>
2021-11-12 13:53:36 +0100

    Feature/a1 mtdc@157 refactor compose (#252)
    Merged docker-compose files as a base for Provider connector to contact PRS API.
    
    Also cleaned up some POM files.

--- coreservices/partsrelationshipservice/connector/run-integration-test.sh
+++ coreservices/partsrelationshipservice/connector/run-integration-test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -euo pipefail
+cd ..
 export DOCKER_BUILDKIT=1
-docker-compose build --build-arg PRS_EDC_PKG_USERNAME=$PRS_EDC_PKG_USERNAME --build-arg PRS_EDC_PKG_PASSWORD=$PRS_EDC_PKG_PASSWORD
-docker-compose up --exit-code-from=integration-test --abort-on-container-exit
+docker-compose --profile connector build --build-arg PRS_EDC_PKG_USERNAME=$PRS_EDC_PKG_USERNAME --build-arg PRS_EDC_PKG_PASSWORD=$PRS_EDC_PKG_PASSWORD 
+docker-compose --profile connector up --exit-code-from=connector-integration-test --abort-on-container-exit
