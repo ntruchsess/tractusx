4150082fee275120c491c0d26a02c4674046f8dc
Marc Gomez <marcgomez@microsoft.com>
2021-11-29 14:32:09 +0100

    Cleanup unused properties and parameters (#309)

--- coreservices/partsrelationshipservice/dev/connector-integration-test.sh
+++ coreservices/partsrelationshipservice/dev/connector-integration-test.sh
@@ -10,13 +10,16 @@ chmod +x retry
 ./wait-for-it.sh -t 60 provider:8181
 ./wait-for-it.sh -t 60 consumer:8181
 ./wait-for-it.sh -t 60 prs:8080
-requestId=$(curl -f -X POST http://consumer:8181/api/v0.1/file -H "Content-type:application/json" -d '{"connectorAddress": "http://provider:8181", "partsTreeRequest": {
-                "oneIDManufacturer": "BMW MUC", "objectIDManufacturer": "YS3DD78N4X7055320", "view": "AS_BUILT", "aspect": "MATERIAL", "depth": 2}}')
 
+# Send request to consumer connector
+requestId=$(curl -f -X POST http://consumer:8181/api/v0.1/file -H "Content-type:application/json" -d '{"partsTreeRequest": {
+    "oneIDManufacturer": "BMW MUC", "objectIDManufacturer": "YS3DD78N4X7055320", "view": "AS_BUILT", "aspect": "MATERIAL", "depth": 2}}')
+
+# Poll status endpoint until job completed
 stateUrl="http://consumer:8181/api/v0.1/datarequest/$requestId/state"
-
 ./retry -s 1 -t 120 "test \$(curl -f -o /dev/null -s -w '%{http_code}' $stateUrl) == 200"
 
+# Get results using SAS URL
 sasUrl=$(curl -f $stateUrl)
 curl "$sasUrl"
 curl "$sasUrl" | grep "relationships\":"
