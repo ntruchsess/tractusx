9b6525e960c62f670b2d7ca153b84514bb9cf8fd
Tobi <tobias.schwarz@microsoft.com>
2021-12-13 08:55:38 +0100

    renaming of Onboarding to Registration Service

---
+++ coreservices/registration/src/CatenaX.NetworkServices.Registration.Service/CDQ/CDQAccessMock.cs
@@ -0,0 +1,39 @@
+﻿using CatenaX.NetworkServices.Registration.Service.CDQ.Model;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatenaX.NetworkServices.Registration.Service.CDQ
+{
+    public class CDQAccessMock : ICDQAccess
+    {
+        public List<FetchBusinessPartnerDto> fetchBusinessPartners = new List<FetchBusinessPartnerDto>()
+        {
+            new FetchBusinessPartnerDto
+            {
+                cdqId = "CDQ:12345",
+                dataSource = "someDataSource",
+                businessPartner = new Businesspartner
+                {
+                    identifiers = new Identifier[]
+                    {
+                        new Identifier
+                        {
+                            type = new Type
+                            {
+                                technicalKey = "CX_BPN",
+                            },
+                            value = "BPN12345"
+                        }
+                    }
+                }
+            }
+        };
+
+        public Task<List<FetchBusinessPartnerDto>> FetchBusinessPartner(string companyIdentifier)
+        {
+            return Task.FromResult(fetchBusinessPartners.Where(d => d.cdqId.Equals(companyIdentifier)).ToList());
+        }
+    }
+}
