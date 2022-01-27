8ecfbdf3c2843113ed9c142a2521ac32469bb92d
Martin Raepple <martin.raepple@microsoft.com>
2022-01-27 09:17:05 +0100

    Feature/cax 12 devops@cplp 127 two keycloak instances (#397)
    * Test new terraform deployment with two IAM DBs
    
    * New test
    
    * added new manifests
    
    * Feature/cax 12 devops@cplp 127 two keycloak instances adjustments evelyn (#348)
    
    * Adjusted naming of keykloak instances
    
    * Adjusted iam workflow to work in onboarding/tractusx repo
    
    * Adjusted indentation for iam workflow
    
    * Added new DBs for KeyCloak
    
    * Modify terraform - use speedboat (#360)
    
    * Update terraform.yml
    
    * enable terraform config job
    
    * update storage account and state file name
    
    * Upgraded K8S and cert-manager to latest version
    
    * Changed cert-manager
    
    * Skip tf_config
    
    * Bugfix
    
    * re-added config step
    
    * Added new keycloak instances to github actions
    
    * included new manifests
    
    * added feature branch
    
    * Fix url to host
    
    * Fixed typo
    
    * Fixed typo
    
    * Fixed typo
    
    * Fixed typo
    
    * fixed db connection
    
    * fixed lowercase for db user
    
    * ACR integration
    
    * changed docker context
    
    * Changed context
    
    * Reduced steps
    
    * Removed feature branch
    
    * Removed deprecated branch
    
    * Removed feature and deprecated team branches
    
    Co-authored-by: Evelyn Gurschler <e.gurschler@mailbox.org>
    Co-authored-by: Csaba Fabian <csaba.fabian@t-systems.com>

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -93,8 +93,8 @@ module "aks_services" {
   location                         = azurerm_resource_group.default_rg.location
   # client_id                        = "your-service-principal-client-appid"
   # client_secret                    = "your-service-principal-client-password"
-  kubernetes_version               = "1.20.7"
-  orchestrator_version             = "1.20.7"
+  kubernetes_version               = "1.22.4"
+  orchestrator_version             = "1.22.4"
   prefix                           = "${var.prefix}-${var.environment}-aks-services"
   cluster_name                     = "${var.prefix}-${var.environment}-aks-services"
   dns_prefix                       = "${var.prefix}${var.environment}akssrv"
@@ -278,7 +278,7 @@ resource "kubernetes_namespace" "cert_manager_namespace" {
   }
 }
 
-# Deploy cert-manager for TLS with Helm
+# Deploy cert-manager with Helm
 resource "helm_release" "cert-manager" {
   name       = "cert-manager"
   chart      = "cert-manager"
