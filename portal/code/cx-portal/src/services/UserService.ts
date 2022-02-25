bde2580f7773eb5df6ebc2deda169c14f6f67d9a
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2022-02-25 13:43:15 +0100

    clean up UserService

--- portal/code/cx-portal/src/services/UserService.ts
+++ portal/code/cx-portal/src/services/UserService.ts
@@ -1,31 +1,21 @@
 import Keycloak from 'keycloak-js'
 
-const CX_REALM = 'CX-Central'
-const CX_CLIENT = 'catenax-portal'
-const CX_ADMIN_ROLE = 'CX Admin'
+const CX_ROLES = {
+  ADMIN: 'CX Admin'
+}
 
-const realm = CX_REALM
-const clientId = CX_CLIENT
-
-//const _kc = new Keycloak('/keycloak.json');
-
-const kcConfig = {
+const keycloakConfig = {
   url: process.env.REACT_APP_KEYCLOAK_URL,
-  realm: CX_REALM,
-  clientId: CX_CLIENT,
+  realm: 'CX-Central',
+  clientId: 'catenax-portal',
   'ssl-required': 'external',
   'public-client': true,
 }
 
-const _kc = new (Keycloak as any)(kcConfig)
+const KC = new (Keycloak as any)(keycloakConfig)
 
-/**
- * Initializes Keycloak instance and calls the provided callback function if successfully authenticated.
- *
- * @param onAuthenticatedCallback
- */
 const initKeycloak = (onAuthenticatedCallback: Function) => {
-  _kc
+  KC
     .init({
       onLoad: 'login-required',
       silentCheckSsoRedirectUri:
@@ -41,65 +31,48 @@ const initKeycloak = (onAuthenticatedCallback: Function) => {
     })
 }
 
-const doLogin = _kc.login
+const doLogin = KC.login
 
-const doLogout = _kc.logout
+const doLogout = KC.logout
 
-//forward as header "authentication: Bearer ${getToken()}"
-const getToken = () => _kc.token
+const getToken = () => KC.token
 
-const getParsedToken = () => _kc.tokenParsed
-
-const isLoggedIn = () => !!_kc.token
+const getParsedToken = () => KC.tokenParsed
 
 const updateToken = (successCallback: Function) =>
-  _kc.updateToken(5).then(successCallback).catch(doLogin)
+  KC.updateToken(5).then(successCallback).catch(doLogin)
 
-const getUsername = () => _kc.tokenParsed?.preferred_username
+const getUsername = () => KC.tokenParsed.preferred_username
 
-const getName = () => _kc.tokenParsed?.name
+const getName = () => KC.tokenParsed?.name
 
-const getEmail = () => _kc.tokenParsed?.email
+const getEmail = () => KC.tokenParsed?.email
 
-const getInitials = () =>
-  _kc.tokenParsed?.name
-    .split(/[ -_.@]/)
-    .reduce((a: string, b: string) => a + b[0], '')
-    .substring(0, 2)
-    .toUpperCase()
+const getCompany = () => KC.tokenParsed?.organisation
 
-const getDomain = () => CX_REALM
-
-const getTenant = () => _kc.tokenParsed?.tenant
-
-const getCompany = () => _kc.tokenParsed?.organisation
-
-const getRoles = () => _kc.tokenParsed?.resource_access[CX_CLIENT]?.roles
+const getRoles = () => KC.tokenParsed?.resource_access[keycloakConfig.clientId]?.roles
 
 const hasRole = (role: string) => getRoles()?.includes(role)
 
-const isAdmin = () => hasRole(CX_ADMIN_ROLE)
+const isAdmin = () => hasRole(CX_ROLES.ADMIN)
+
+const isLoggedIn = () => !!KC.token
 
 const UserService = {
-  initKeycloak,
   doLogin,
   doLogout,
-  isLoggedIn,
   getToken,
   getParsedToken,
-  updateToken,
   getEmail,
   getUsername,
   getName,
-  getInitials,
-  getDomain,
-  getTenant,
   getCompany,
   getRoles,
-  isAdmin,
   hasRole,
-  realm,
-  clientId,
+  initKeycloak,
+  isAdmin,
+  isLoggedIn,
+  updateToken,
 }
 
 export default UserService
