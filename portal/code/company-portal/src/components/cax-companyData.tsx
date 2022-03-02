3dad2e570420010dadf7dfb93de603ba24b66c1a
jjeroch <94133633+jjeroch@users.noreply.github.com>
2022-03-02 22:57:23 +0100

    Update cax-companyData.tsx
    Commented out the previous data mapping and updated to the new/latest data mapping

--- portal/code/company-portal/src/components/cax-companyData.tsx
+++ portal/code/company-portal/src/components/cax-companyData.tsx
@@ -51,12 +51,18 @@ export const CompanyDataCax = ({currentActiveStep, addCurrentStep}: CompanyDataP
         const companyDetails = await getCompanyDetails(x);
         setcompanyDetails(companyDetails);
         setbpn(companyDetails?.[0]?.bpn);
-        setlegalEntity(companyDetails?.[0]?.names.find(x => x.type === 'INTERNATIONAL')?.value);
-        setregisteredName(companyDetails?.[0]?.names.find(x => x.type === 'REGISTERED')?.value);
-        setstreetHouseNumber(companyDetails?.[0]?.addresses?.[0]?.thoroughfares.find(x => x.type === 'INDUSTRIAL_ZONE')?.value);
-        setpostalCode(companyDetails?.[0]?.addresses?.[0]?.postCodes.find(x => x.type === 'REGULAR')?.value);
-        setcity(companyDetails?.[0]?.addresses?.[0]?.localities.find(x => x.type === 'BLOCK')?.value);
-        setcountry(companyDetails?.[0]?.addresses?.[0]?.countryCode);
+        //setlegalEntity(companyDetails?.[0]?.names.find(x => x.type === 'INTERNATIONAL')?.value);
+        //setregisteredName(companyDetails?.[0]?.names.find(x => x.type === 'REGISTERED')?.value);
+        //setstreetHouseNumber(companyDetails?.[0]?.addresses?.[0]?.thoroughfares.find(x => x.type === 'INDUSTRIAL_ZONE')?.value);
+        //setpostalCode(companyDetails?.[0]?.addresses?.[0]?.postCodes.find(x => x.type === 'REGULAR')?.value);
+        //setcity(companyDetails?.[0]?.addresses?.[0]?.localities.find(x => x.type === 'BLOCK')?.value);
+        //setcountry(companyDetails?.[0]?.addresses?.[0]?.countryCode);
+        setlegalEntity(companyDetails?.[0]?.names?.[0]?.value);
+        setregisteredName(companyDetails?.[0]?.names?.[0]?.value);
+        setstreetHouseNumber(companyDetails?.[0]?.addresses?.[0]?.thoroughfares.[0]?.value);
+        setpostalCode(companyDetails?.[0]?.addresses?.[0]?.postCodes.[0]?.value);
+        setcity(companyDetails?.[0]?.addresses?.[0]?.localities.[0]?.value);
+        setcountry(companyDetails?.[0]?.addresses?.[0]?.country?.[0]?.name);
     }
 
     const backClick = () => {
