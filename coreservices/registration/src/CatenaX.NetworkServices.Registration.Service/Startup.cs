9c8e71aff28b8e8b3a603ff619afd218fb3879db
Evelyn Gurschler <evelyn.gurschler@bmw.de>
2022-03-23 15:51:59 +0100

    enabled debugging

--- coreservices/registration/src/CatenaX.NetworkServices.Registration.Service/Startup.cs
+++ coreservices/registration/src/CatenaX.NetworkServices.Registration.Service/Startup.cs
@@ -123,7 +123,6 @@ namespace CatenaX.NetworkServices.Registration.Service
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
-            
             if (Configuration.GetValue<bool?>("DebugEnabled") != null && Configuration.GetValue<bool>("DebugEnabled"))
             {
                 app.UseDeveloperExceptionPage();
