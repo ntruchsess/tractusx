0f7fe2897987579ff46a2c3c8b5a5b22396e51c1
Evelyn Gurschler <evelyn.gurschler@bmw.de>
2022-03-23 15:43:39 +0100

    enabled debugging

--- coreservices/registration/src/CatenaX.NetworkServices.Registration.Service/Startup.cs
+++ coreservices/registration/src/CatenaX.NetworkServices.Registration.Service/Startup.cs
@@ -123,7 +123,8 @@ namespace CatenaX.NetworkServices.Registration.Service
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
-            if (env.IsDevelopment())
+            
+            if (Configuration.GetValue<bool?>("DebugEnabled") != null && Configuration.GetValue<bool>("DebugEnabled"))
             {
                 app.UseDeveloperExceptionPage();
                 KeycloakUntrustedCertExceptionHandler.ConfigureExceptions(Configuration.GetSection("Keycloak"));
