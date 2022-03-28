731a51980fca4f31fecead60557ebfba98fa63d7
Christian Wölk <christian.woelk@raysono.com>
2022-03-28 09:49:31 +0200

    CPLP-303: Remove unused i18n

--- portal/code/cx-portal/src/components/pages/UserManagement/index.tsx
+++ portal/code/cx-portal/src/components/pages/UserManagement/index.tsx
@@ -1,9 +1,6 @@
-import { useTranslation } from 'react-i18next'
 import { ActiveUserTable } from './ActiveUserTable'
 
 export default function UserManagement() {
-  const { t } = useTranslation()
-
   const onAddUserButtonClick = () => {
     console.log('add new user')
   }
