5e13b318e772427c84643585494b444af5baaf9e
Fatih Ayyildiz <fatihhikmetayyildiz@gmail.com>
2022-03-29 16:14:26 +0200

    yarn prettier command run

--- portal/code/cx-portal-shared-components/src/components/basic/Chip/chip.stories.tsx
+++ portal/code/cx-portal-shared-components/src/components/basic/Chip/chip.stories.tsx
@@ -8,14 +8,12 @@ import {
   ArgsTable,
   Stories,
   PRIMARY_STORY,
-} from '@storybook/addon-docs';
+} from '@storybook/addon-docs'
 
 export default {
   title: 'Chip',
   component: Component,
-  argTypes: {
-
-  },
+  argTypes: {},
   parameters: {
     docs: {
       page: () => (
@@ -23,11 +21,24 @@ export default {
           <h1>Catena-X Chip Component</h1>
           <p>
             Chip component used the same way as described in the
-            <a href='https://mui.com/components/chips/'
-               target='_blank' rel="noreferrer">MUI Chip</a> documentation with
-            <a href='https://mui.com/api/chips/' target='_blank' rel="noreferrer">MUI Chip API</a>
-            <p style={{color:"red"}}>
-              Please take a look to "Examples" stories doc page for example usages
+            <a
+              href="https://mui.com/components/chips/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              MUI Chip
+            </a>{' '}
+            documentation with
+            <a
+              href="https://mui.com/api/chips/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              MUI Chip API
+            </a>
+            <p style={{ color: 'red' }}>
+              Please take a look to "Examples" stories doc page for example
+              usages
             </p>
           </p>
           <Subtitle />
--- portal/code/cx-portal-shared-components/src/components/basic/Dropzone/components/Layout.tsx
+++ portal/code/cx-portal-shared-components/src/components/basic/Dropzone/components/Layout.tsx
@@ -1,14 +1,21 @@
-import { ILayoutProps } from "react-dropzone-uploader";
+import { ILayoutProps } from 'react-dropzone-uploader'
 import { Box } from '@mui/material'
 
-export const Layout = ({ input, previews, dropzoneProps, files, extra: { maxFiles } }: ILayoutProps) => {
-
+export const Layout = ({
+  input,
+  previews,
+  dropzoneProps,
+  files,
+  extra: { maxFiles },
+}: ILayoutProps) => {
   return (
-    <Box sx={{
-      '.dzu-input': {
-        visibility: 'hidden'
-      }
-    }}>
+    <Box
+      sx={{
+        '.dzu-input': {
+          visibility: 'hidden',
+        },
+      }}
+    >
       {previews}
       <div {...dropzoneProps}>{files.length < maxFiles && input}</div>
     </Box>
