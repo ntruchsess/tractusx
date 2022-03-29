abc57309db64631a873558793673dee0ed694a6b
Diana Renz <diana-leontyeva@mail.ru>
2022-03-29 18:15:38 +0200

    add base for Navbar and Stage Section

--- portal/code/cx-portal/src/components/pages/Dashboard/index.tsx
+++ portal/code/cx-portal/src/components/pages/Dashboard/index.tsx
@@ -1,9 +1,10 @@
 import SearchSection from './components/SearchSection'
 import StageSection from './components/StageSection'
+import './dashboard.scss'
 
 export default function Dashboard() {
   return (
-    <main>
+    <main className='dashboard'>
       <StageSection />
       <SearchSection />
     </main>
